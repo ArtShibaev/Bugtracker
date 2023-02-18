8e19c2c4f0398698dbbcf9a15a3d315e7184f3f2
Ra1nbow <motvey.romanov@yandex.ru>
2023-02-19 00:58:12 +0300

    Регистрация вынесена в отдельный компонент

---
+++ registration_form.py
@@ -0,0 +1,36 @@
+import os
+from dotenv import load_dotenv
+
+from PySide6 import QtCore, QtWidgets
+from PySide6.QtUiTools import QUiLoader
+from pymongo import MongoClient
+
+load_dotenv('.env')
+
+
+loader = QUiLoader()
+mongo_url = os.environ.get('MONGO_URL')
+
+def find_user(login):
+    client = MongoClient(mongo_url)
+    db = client['bugtracker']
+    users = db['users']
+    user = users.find_one({'login': login})
+
+    return user
+
+class RegistrationForm(QtCore.QObject):
+    def __init__(self):
+        super().__init__()
+        self.ui = loader.load('registration_page.ui', None)
+        self.ui.submit_button_registration.clicked.connect(self.registration_data)
+    def show(self):
+        self.ui.show()
+    def registration_data(self):
+        login = self.ui.input_login_registration
+        password = self.ui.input_password_registration
+        password_sec = self.ui.input_password_registration_second_time
+        if login.text() == '': login.setPlaceholderText("Введите логин!")
+        if password.text() != password_sec.text(): password_sec.setText(''); password_sec.setPlaceholderText("Пароли не совпадают!")
+        elif password.text() == '': password.setPlaceholderText("Введите пароль!")
+        else: print('Данные пользвателя: ' + login.text() + ':' + password.text()); self.ui.close()
