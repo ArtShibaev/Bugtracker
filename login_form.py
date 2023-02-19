ca18fa43b3624f8694a97c186f931d97970b4900
Ra1nbow <motvey.romanov@yandex.ru>
2023-02-19 22:06:38 +0300

    Добавлены стили для невалидных полей ввода

--- login_form.py
+++ login_form.py
@@ -15,9 +15,6 @@ loader = QUiLoader()
 mongo_url = os.environ.get('MONGO_URL')
 
 def find_user(login):
-    client = MongoClient(mongo_url)
-    db = client['bugtracker']
-    users = db['users']
     user = users.find_one({'login': login})
 
     return user
@@ -29,27 +26,34 @@ class LoginForm(QtCore.QObject):
 
         self.ui.input_password.setEchoMode(self.ui.input_password.EchoMode.Password)
         self.ui.show_password.stateChanged.connect(self.showHidePassword)
-        self.ui.login_button.clicked.connect(self.login())
+        self.ui.login_button.clicked.connect(self.login)
         self.ui.register_button.clicked.connect(self.goToRegistration)
 
     def show(self):
         self.ui.show()
     # Тестовые данные login:pass - 123:321
     def login(self):
-        print(self.ui.input_login.text() + ':' + self.ui.input_password.text())
+
+        self.setBorderColor(self.ui.input_login, 'gray')
+        self.setBorderColor(self.ui.input_password, 'gray')
+
         current_user = find_user(self.ui.input_login.text())
 
         # Проверка на нахождения логина в базе данных
         if current_user is None:
-            print('Пользователь не найден')
+            self.setBorderColor(self.ui.input_login, 'red')
         elif current_user['password'] == self.ui.input_password.text():
             print('Выполнен вход')
         else:
-            print('Пароль неверный')
+            self.setBorderColor(self.ui.input_password, 'red')
+
 
     def showHidePassword(self):
         if self.ui.show_password.isChecked(): self.ui.input_password.setEchoMode(self.ui.input_password.EchoMode.Normal)
         else: self.ui.input_password.setEchoMode(self.ui.input_password.EchoMode.Password)
+
+    def setBorderColor(self, input, color):
+        input.setStyleSheet('QLineEdit {border: 2px solid ' + color + ';color: white;border-radius: 15px;padding: 5px 10px;outline: none;focus {border-color: rgb(130, 118, 255);}}')
 
     def goToRegistration(self):
         # Скрываем интерфейс логина
