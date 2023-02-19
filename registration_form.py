6f5a0ce959227d0c695fc458edcad70dba1f3712
Ra1nbow <motvey.romanov@yandex.ru>
2023-02-19 22:27:32 +0300

    Бд подключена к регистрации

--- registration_form.py
+++ registration_form.py
@@ -1,9 +1,22 @@
+import os
+
 from PySide6 import QtCore
 from PySide6.QtGui import QRegularExpressionValidator
 from PySide6.QtUiTools import QUiLoader
 import re
+import time
+import hashlib
+from dotenv import load_dotenv
+from pymongo import MongoClient
+
+load_dotenv('.env')
 
 loader = QUiLoader()
+
+mongo_url = os.environ.get('MONGO_URL')
+client = MongoClient(mongo_url)
+db = client['bugtracker']
+users = db['users']
 
 class RegistrationForm(QtCore.QObject):
     def __init__(self):
@@ -11,16 +24,9 @@ class RegistrationForm(QtCore.QObject):
         self.ui = loader.load('./interfaces/registration_form.ui', None)
         self.ui.submit_button.clicked.connect(self.register)
         self.ui.login_button.clicked.connect(self.goToLogin)
+
     def show(self):
         self.ui.show()
-    def register(self):
-        if self.validator():
-            print('Данные пользователя: ' + f'login: {self.ui.input_login.text()}, email: {self.ui.input_email.text()}, pass: {self.ui.input_password.text()}')
-            self.ui.close()
-
-    # Функция изменяет цвет границы на color для элемента input
-    def setBorderColor(self, input, color):
-        input.setStyleSheet('QLineEdit {border: 2px solid ' + color + ';color: white;border-radius: 15px;padding: 5px 10px;outline: none;focus {border-color: rgb(130, 118, 255);}}')
 
     def validator(self):
         # При каждом нажатии кнопки нормализуем все цвета бордера
@@ -49,6 +55,24 @@ class RegistrationForm(QtCore.QObject):
             flag = False
 
         return flag
+
+    def register(self):
+        if self.validator():
+            users.insert_one({
+                "login": self.ui.input_login.text(),
+                # Хэширование пароля
+                "password": hashlib.sha256(self.ui.input_password.text().encode('utf-8')).hexdigest(),
+                "email": self.ui.input_email.text(),
+                # Время в Unix
+                "registrationDate": round(time.time()*1000)
+            })
+        self.ui.close()
+
+    # Функция изменяет цвет границы на color для элемента input
+    def setBorderColor(self, input, color):
+        input.setStyleSheet('QLineEdit {border: 2px solid ' + color + ';color: white;border-radius: 15px;padding: 5px 10px;outline: none;focus {border-color: rgb(130, 118, 255);}}')
+
+
 
     def goToLogin(self):
         # Да, это импорт посередине кода. Если указать его сверху, то компилятор распознает его как зацикленный
