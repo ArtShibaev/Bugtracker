962a69dcfb7e5f735e1952ebb7034803fd481367
Ra1nbow <motvey.romanov@yandex.ru>
2023-02-23 15:04:00 +0300

    Подправил интерфейс Но делать еще дохера просто :/

--- welcome_page_form.py
+++ welcome_page_form.py
@@ -7,10 +7,13 @@ loader = QUiLoader()
 
 class WelcomePageForm(QtCore.QObject):
 
-    def __init__(self):
+    def __init__(self, login):
         super().__init__()
         self.ui = loader.load('./interfaces/welcome_page.ui', None)
+
         Images.load_image(self)
+
+        self.ui.welcome_user.setText(f'Привет, {login}!')
 
     def show(self):
         self.ui.show()
