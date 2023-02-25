67d0286f61c87bacb9109be0261b40e310ff1ac0
Ra1nbow <motvey.romanov@yandex.ru>
2023-02-25 19:12:40 +0300

    Изменил карточки и лэйаут Добавил контрастирующий с фоном цвет текста

--- bug_card.py
+++ bug_card.py
@@ -1,8 +1,9 @@
 import os
 import random
+import re
 
 from PySide6.QtUiTools import QUiLoader
-from PySide6.QtWidgets import QWidget
+from PySide6.QtWidgets import QWidget, QLabel
 from pymongo import MongoClient
 
 loader = QUiLoader()
@@ -15,12 +16,14 @@ def findUser(uid):
     user = users.find_one({'uid': uid})
     return user
 
+# (градиент, стили_для_текста)
 backgrounds = [
-    'background: qlineargradient(spread:repeat, x1:0.5, y1:0, x2:0.5, y2:1, stop:0 rgba(108, 87, 193, 255), stop:1 rgba(72, 38, 138, 255));border-radius: 14px;}',
-    'background: qlineargradient(spread:repeat, x1:0.5, y1:0, x2:0.5, y2:1, stop:0 rgba(253, 225, 159, 255), stop:1 rgba(218, 189, 88, 255));border-radius: 14px;}',
-    'background: qlineargradient(spread:repeat, x1:0.5, y1:0, x2:0.5, y2:1, stop:0 rgba(169, 235, 216, 255), stop:1 rgba(102, 197, 157, 255));border-radius: 14px;}',
+    ('background: qlineargradient(spread:repeat, x1:0.5, y1:0, x2:0.5, y2:1, stop:0 rgba(108, 87, 193, 255), stop:1 rgba(72, 38, 138, 255));border-radius: 14px;}', 'color:white;background:transparent;'),
+    ('background: qlineargradient(spread:repeat, x1:0.5, y1:0, x2:0.5, y2:1, stop:0 rgba(253, 225, 159, 255), stop:1 rgba(218, 189, 88, 255));border-radius: 14px;}', 'color:black;background:transparent;'),
+    ('background: qlineargradient(spread:repeat, x1:0.5, y1:0, x2:0.5, y2:1, stop:0 rgba(169, 235, 216, 255), stop:1 rgba(102, 197, 157, 255));border-radius: 14px;}', 'color:black;background:transparent;'),
 
 ]
+
 class BugCard(QWidget):
     def __init__(self, title, date, author_id, assignee_id, tags, criticality):
         super().__init__()
@@ -29,8 +32,10 @@ class BugCard(QWidget):
         file = loader.load('./interfaces/bug_card.ui')
         layout = file.layout()
         self.setLayout(layout)
-        # TODO: Это почему-то не работает
-        file.setStyleSheet(random.choice(backgrounds))
+        colors = random.choice(backgrounds)
+        file.frame_4.setStyleSheet(colors[0])
+        for x in self.findChildren(QLabel):
+            x.setStyleSheet(colors[1])
         file.title.setText(title)
         file.date.setText(date)
         file.author.setText(findUser(author_id)['login'])
@@ -46,6 +51,6 @@ class BugCard(QWidget):
 
         tags_string = ', '.join([tag for tag in tags])
 
-        file.tags.setText(tags_string.rstrip())
+        file.tags.setText(tags_string)
 
         self.setFixedSize(240, 160)
