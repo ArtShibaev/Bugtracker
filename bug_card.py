2ed0977423abcbf6607c9b298a2f373efe9ea354
Ra1nbow <motvey.romanov@yandex.ru>
2023-02-25 12:36:29 +0300

    Подключил виджеты карточек Нужно кое-что пофиксить

---
+++ bug_card.py
@@ -0,0 +1,51 @@
+import os
+import random
+
+from PySide6.QtUiTools import QUiLoader
+from PySide6.QtWidgets import QWidget
+from pymongo import MongoClient
+
+loader = QUiLoader()
+mongo_url = os.environ.get('MONGO_URL')
+client = MongoClient(mongo_url)
+db = client['bugtracker']
+users = db['users']
+
+def findUser(uid):
+    user = users.find_one({'uid': uid})
+    return user
+
+backgrounds = [
+    'background: qlineargradient(spread:repeat, x1:0.5, y1:0, x2:0.5, y2:1, stop:0 rgba(108, 87, 193, 255), stop:1 rgba(72, 38, 138, 255));border-radius: 14px;}',
+    'background: qlineargradient(spread:repeat, x1:0.5, y1:0, x2:0.5, y2:1, stop:0 rgba(253, 225, 159, 255), stop:1 rgba(218, 189, 88, 255));border-radius: 14px;}',
+    'background: qlineargradient(spread:repeat, x1:0.5, y1:0, x2:0.5, y2:1, stop:0 rgba(169, 235, 216, 255), stop:1 rgba(102, 197, 157, 255));border-radius: 14px;}',
+
+]
+class BugCard(QWidget):˚
+    def __init__(self, title, date, author_id, assignee_id, tags, criticality):
+        super().__init__()
+
+        # Set up the widget's layout
+        file = loader.load('./interfaces/bug_card.ui')
+        layout = file.layout()
+        self.setLayout(layout)
+        # TODO: Это почему-то не работает
+        file.setStyleSheet(random.choice(backgrounds))
+        file.title.setText(title)
+        file.date.setText(date)
+        file.author.setText(findUser(author_id)['login'])
+        file.assignee.setText(findUser(assignee_id)['login'])
+
+        if criticality == 'high':
+            file.criticality.setStyleSheet('background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:1, stop:0 rgba(251, 61, 61, 255), stop:1 rgba(152, 38, 38, 255));border-radius: 14px;')
+        elif criticality == 'medium':
+            file.criticality.setStyleSheet('background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:1, stop:0 rgba(255, 149, 26, 255), stop:1 rgba(255, 187, 13, 255));border-radius: 14px;')
+        elif criticality == 'low':
+            file.criticality.setStyleSheet('background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:1, stop:0 rgba(16, 148, 53, 255), stop:1 rgba(29, 215, 81, 255));border-radius: 14px;')
+
+
+        tags_string = ', '.join([tag for tag in tags])
+
+        file.tags.setText(tags_string.rstrip())
+
+        self.setFixedSize(240, 160)
--- main_page.py
+++ main_page.py
@@ -1,10 +1,12 @@
 from PySide6 import QtCore, QtGui
 from PySide6.QtUiTools import QUiLoader
 from pymongo import MongoClient
+from datetime import datetime
 
 import os
 from dotenv import load_dotenv
 
+from bug_card import BugCard
 from image_loader import Images
 
 load_dotenv('.env')
@@ -24,6 +26,8 @@ class MainPage(QtCore.QObject):
         super().__init__()
         self.ui = loader.load('./interfaces/main_page.ui', None)
         self.fillingProjectList(uid)
+        certainProject = projects.find_one({'owner': uid})
+        self.fillBugCards(certainProject)
 
         Images.load_image(self, 'main_page')
 
@@ -33,5 +37,10 @@ class MainPage(QtCore.QObject):
     def fillingProjectList(self, uid):
         res = list(projects.find({'owner': uid}))
         for project in res:
-            print(project["title"])
             self.ui.projects_list.addItem(project["title"])
+
+    def fillBugCards(self, project):
+        for bug in project['bugs']:
+            bug = BugCard(bug['title'], datetime.utcfromtimestamp(bug['createdDate']/1000).strftime('%d.%m.%Y %H:%M'), bug['author'], bug['assignee'], bug['tags'], bug['criticality'])
+            self.ui.bug_cards.addWidget(bug)
+
