f3962a07cee298f3effaa99431be3e367691b23c
Ra1nbow <motvey.romanov@yandex.ru>
2023-02-25 11:26:04 +0300

    Оптимизирован лоадер Предлагаю такой вариант

--- image_loader.py
+++ image_loader.py
@@ -1,10 +1,17 @@
 from PySide6 import QtCore, QtGui
+import os
 
 class Images(QtCore.QObject):
 
-    def load_image(self):
-        self.ui.new_project.setIcon(QtGui.QIcon('./images/plus.png'))
-        self.ui.home.setIcon(QtGui.QIcon('./images/main_page.png'))
-        self.ui.settings.setIcon(QtGui.QIcon('./images/gear.png'))
-        self.ui.sp_new_project.setIcon(QtGui.QIcon('./images/plus.png'))
-        self.ui.users_photo.setIcon(QtGui.QIcon('./images/user_icon.png'))
+    def load_image(self, file):
+        if file == 'welcome_page':
+            self.ui.new_project.setIcon(QtGui.QIcon('./images/plus.png'))
+            self.ui.home.setIcon(QtGui.QIcon('./images/main_page.png'))
+            self.ui.settings.setIcon(QtGui.QIcon('./images/gear.png'))
+            self.ui.sp_new_project.setIcon(QtGui.QIcon('./images/plus.png'))
+            self.ui.users_photo.setIcon(QtGui.QIcon('./images/user_icon.png'))
+        elif file == 'main_page':
+            self.ui.new_project.setIcon(QtGui.QIcon('./images/plus.png'))
+            self.ui.home.setIcon(QtGui.QIcon('./images/main_page.png'))
+            self.ui.settings.setIcon(QtGui.QIcon('./images/gear.png'))
+            self.ui.users_photo.setIcon(QtGui.QIcon('./images/user_icon.png'))
