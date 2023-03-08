0e486e6c594cdc9b701031eebece17426a636056
Artem Shibaev <artem.shibaev.dm@gmail.com>
2023-03-08 17:28:22 +0300

    Добавлена функция приглашения пользователя в команду - Реализованы различные проверки логина ("Логин не найден", "Пользователь уже в команде" и т.д.) - Если команда создана в очном проекте, его id меняется на id вида "t_НаборЦифр" - При создании команды, ее поле добавляется в БД - Добавлять пользователей в команду может только админ

--- main_page.py
+++ main_page.py
@@ -44,15 +44,15 @@ backgrounds = [
     ('background: qlineargradient(spread:repeat, x1:0.5, y1:0, x2:0.5, y2:1, stop:0 rgba(169, 235, 216, 255), stop:1 rgba(102, 197, 157, 255));border-radius: 14px;}', 'color:black;background:transparent;font-size:14px;'),
 ]
 
+project_l = []
+
 class MainPage(QtCore.QObject):
     def __init__(self, uid, login):
         super().__init__()
         self.ui = loader.load('./interfaces/main_page.ui', None)
         self.ui_create_project = loader.load('./interfaces/create_new_project_form.ui', None)
         self.ui_create_card = loader.load('./interfaces/new_bug_card.ui', None)
-
-        self.ui.new_project.clicked.connect(self.createNewProject)
-        self.ui.create_card.clicked.connect(self.createNewBugCard)
+        self.ui_new_member = loader.load('./interfaces/new_member.ui', None)
 
         self.user_login = login
 
@@ -63,6 +63,10 @@ class MainPage(QtCore.QObject):
             for team in list(teams.find({})):
                 if uid == team['admin']:
                     self.certainProject = projects.find_one({'owner': team['tid']})
+
+        self.ui.new_project.clicked.connect(self.createNewProject)
+        self.ui.create_card.clicked.connect(self.createNewBugCard)
+        self.ui.new_member.clicked.connect(self.sendJoinRequrst)
 
         self.ui.projects_list.currentIndexChanged.connect(self.reloadProjectInfo)
 
@@ -76,22 +80,20 @@ class MainPage(QtCore.QObject):
 
         layout = QVBoxLayout()
         layout.setAlignment(Qt.AlignTop)
+        pixmap = QPixmap()
 
         if project['owner'] == getUserInfo(self.user_login)['uid']:
             url_image = 'https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png'
-            pixmap = QPixmap()
             pixmap.loadFromData(requests.get(url_image).content)
 
             member = QPushButton(QtGui.QIcon(pixmap), textwrap.shorten(self.user_login, 18, placeholder='...'))
             member.setIconSize(QSize(30, 30))
             member.setStyleSheet(Config.membersStyleSheet)
-            # member.clicked.connect()
             layout.addWidget(member)
         else:
             current_team = teams.find_one({'tid': self.certainProject['owner']})
 
             url_image = 'https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png'
-            pixmap = QPixmap()
             pixmap.loadFromData(requests.get(url_image).content)
 
             admin = users.find_one({'uid': current_team['admin']})
@@ -99,11 +101,10 @@ class MainPage(QtCore.QObject):
             member = QPushButton(QtGui.QIcon(pixmap), textwrap.shorten(admin['login'], 18, placeholder='...'))
             member.setIconSize(QSize(30, 30))
             member.setStyleSheet(Config.membersStyleSheet)
-            # member.clicked.connect()
+            #member.clicked.connect(self.showMenu)
             layout.addWidget(member)
             for members in current_team['members']:
                 url_image = 'https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png'
-                pixmap = QPixmap()
                 pixmap.loadFromData(requests.get(url_image).content)
 
                 user = users.find_one({'uid': members})
@@ -111,24 +112,80 @@ class MainPage(QtCore.QObject):
                 member = QPushButton(QtGui.QIcon(pixmap), textwrap.shorten(user['login'], 18, placeholder='...'))
                 member.setIconSize(QSize(30, 30))
                 member.setStyleSheet(Config.membersStyleSheet)
-                # member.clicked.connect()
+                #member.clicked.connect(self.showMenu)
                 layout.addWidget(member)
 
         widget = QWidget()
         widget.setLayout(layout)
         self.ui.team.setWidget(widget)
 
+    #def showMenu(self):
+
+
+    def sendJoinRequrst(self):
+        for x in self.ui.findChildren(QPushButton) + self.ui.findChildren(QComboBox):
+            x.setEnabled(False)
+        self.ui_new_member.show()
+        self.ui_new_member.back.clicked.connect(self.closeSendJoinRequrst)
+        self.ui_new_member.add.clicked.connect(self.addNewUser)
+
+    def closeSendJoinRequrst(self):
+        for x in self.ui.findChildren(QPushButton) + self.ui.findChildren(QComboBox):
+            x.setEnabled(True)
+        self.ui_new_member.close()
+
+    def addNewUser(self):
+        user = getUserInfo(self.ui_new_member.user_login.text())
+
+        if self.ui_new_member.user_login.text() != '':
+            if user is not None:
+                owner = str(self.certainProject['owner'])
+                if owner.startswith('t_'):
+                    team = getUserTeam(getUserInfo(self.user_login)['uid'])
+                    if team is not None:
+                        if user['uid'] not in team['members']:
+                            team['members'].append(user['uid'])
+                            teams.update_one({'tid': team['tid']}, {'$set': {"members": team['members']}})
+                            self.fillingTeamList(self.certainProject)
+                            self.closeSendJoinRequrst()
+                            self.ui_new_member.user_login.clear()
+                            self.ui_new_member.user_login.setPlaceholderText('Логин пользователя')
+                        else: print('Участник уже в команде')
+                    else: print('Вы не обладаете правами администратора')
+                else:
+                    if user['uid'] != getUserInfo(self.user_login)['uid']:
+                        teams.insert_one({
+                            "tid": 't_' + str(random.randrange(111111, 999999, 5)),
+                            "admin": getUserInfo(self.user_login)['uid'],
+                            "members": [],
+                        })
+                        team = getUserTeam(self.certainProject['owner'])
+                        team["members"].append(user['uid'])
+                        teams.update_one({"tid": team['tid']}, {'$set': {"members": team['members']}})
+                        projects.update_one({"title": self.certainProject['title']}, {'$set': {"owner": getUserTeam(getUserInfo(self.user_login)['uid'])['tid']}})
+                        self.fillingTeamList(self.certainProject)
+                        self.closeSendJoinRequrst()
+                        self.ui_new_member.user_login.clear()
+                        self.ui_new_member.user_login.setPlaceholderText('Логин пользователя')
+                    else: print('Участник уже в команде')
+            else: print('Пользователь не найден')
+        else: print('Введите логин пользователя')
 
     def fillingProjectList(self, uid):
+        print(project_l)
         if projects.find_one({'owner': uid}):
             for project in projects.find({'owner': uid}):
-                self.ui.projects_list.addItem(project["title"])
+                if project["title"] not in project_l:
+                    self.ui.projects_list.addItem(project["title"])
+                    project_l.append(project["title"])
 
 
         for team in list(teams.find({})):
             if uid == team['admin'] or uid in team['members']:
                 for project in list(projects.find({'owner': team['tid']})):
-                    self.ui.projects_list.addItem(project["title"])
+                    if project["title"] not in project_l:
+                        self.ui.projects_list.addItem(project["title"])
+                        project_l.append(project["title"])
 
     def createNewBugCard(self):
         for x in self.ui.findChildren(QPushButton) + self.ui.findChildren(QComboBox):
@@ -290,7 +347,6 @@ class MainPage(QtCore.QObject):
         self.certainProject = project
         self.loadBugs(project)
         self.fillingTeamList(self.certainProject)
-
 
     def newProject(self):
         if self.ui_create_project.newproject_name.text() != '':
