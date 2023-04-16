6b040de7f5f54baf742abb83b2820bfc18f98aed
Artem Shibaev <artem.shibaev.dm@gmail.com>
2023-04-16 15:58:41 +0300

    Убраны комментарии

--- bug_page.py
+++ bug_page.py
@@ -15,7 +15,7 @@ from PySide6.QtGui import QPixmap, QColor, QCursor
 from PySide6.QtUiTools import QUiLoader
 from PySide6.QtWidgets import QVBoxLayout, QPushButton, QWidget, QComboBox, QLabel, QCompleter
 from pymongo import MongoClient
-#import pastebinpy as pbp
+import pastebinpy as pbp
 
 
 import config
@@ -498,7 +498,7 @@ class BugPage(QtCore.QObject):
             "steps": self.ui_create_card.reproduction.toPlainText(),
             "complexity": self.ui_create_card.horizontalSlider.value(),
             "messages": [],
-            #"pastebin_link": pbp.paste(os.environ.get('PASTEBIN_KEY'), self.ui_create_card.code_fragment.toPlainText(), self.ui_create_card.title.text(), privacy="1") if self.ui_create_card.code_fragment.toPlainText() else ""
+            "pastebin_link": pbp.paste(os.environ.get('PASTEBIN_KEY'), self.ui_create_card.code_fragment.toPlainText(), self.ui_create_card.title.text(), privacy="1") if self.ui_create_card.code_fragment.toPlainText() else ""
 
 
         })
--- main_page.py
+++ main_page.py
@@ -11,7 +11,7 @@ from PySide6.QtGui import *
 from pymongo import MongoClient
 from itertools import groupby
 import datetime
-#import pastebinpy as pbp
+import pastebinpy as pbp
 
 import os
 from dotenv import load_dotenv
@@ -448,7 +448,7 @@ class MainPage(QtCore.QObject):
             "messages": [],
             "steps": self.ui_create_card.reproduction.toPlainText(),
             "complexity": self.ui_create_card.horizontalSlider.value(),
-            #"pastebin_link": pbp.paste(os.environ.get('PASTEBIN_KEY'), self.ui_create_card.code_fragment.toPlainText(), self.ui_create_card.title.text(), privacy="1") if self.ui_create_card.code_fragment.toPlainText() else ""
+            "pastebin_link": pbp.paste(os.environ.get('PASTEBIN_KEY'), self.ui_create_card.code_fragment.toPlainText(), self.ui_create_card.title.text(), privacy="1") if self.ui_create_card.code_fragment.toPlainText() else ""
         })
 
         if str(project['owner']).startswith('t_'):
