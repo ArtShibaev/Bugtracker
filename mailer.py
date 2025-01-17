7f5c06651ff4f9c3459e909cd2e0ca06a7269f76
Matvey Romanov <youtuberm@yandex.ru>
2025-01-17 18:02:29 +0300

    Убрал пароль

--- mailer.py
+++ mailer.py
@@ -1,25 +1,24 @@
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import smtplib
-
+import os
 
 
 def sendMail(to, subject, message):
     msg = MIMEMultipart()
 
     # Пароль приложения, который нужен для аутентификаци по SMTP
-    password = "emxyavfaftjmqpym"
-    msg['From'] = "bugtrackermailing@gmail.com"
-    # log:pass bugtrackermailing@gmail.com:12345Qweasd
-    msg['To'] = to
-    msg['Subject'] = subject
+    password = os.environ.get("MAIL_PASSWORD")
+    msg["From"] = os.environ.get("MAIL_LOGIN")
+    msg["To"] = to
+    msg["Subject"] = subject
 
-    msg.attach(MIMEText(message, 'html'))
+    msg.attach(MIMEText(message, "html"))
 
-    server = smtplib.SMTP('smtp.gmail.com: 587')
+    server = smtplib.SMTP("smtp.gmail.com: 587")
 
     server.starttls()
-    server.login(msg['From'], password)
-    server.sendmail(msg['From'], msg['To'], msg.as_string())
+    server.login(msg["From"], password)
+    server.sendmail(msg["From"], msg["To"], msg.as_string())
     server.quit()
-    print("Отправлено %s" % (msg['To']))
+    print("Отправлено %s" % (msg["To"]))
